886403690d799ec9086e0959259575fe67f56934
Fikiru Demlew <fikrudemlew@gmail.com>
2026-07-20 19:22:06 +0300

    added footer and detailed page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='blog_index'),
+    path('about/', views.about, name='about'),
     path('create/', views.blog_create, name='blog_create'),
     path('<int:blog_id>/', views.blogs_detail, name='blog_detail'),
     path('<int:blog_id>/edit/', views.blogs_edit, name='blog_edit'),
--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,10 @@ def index(request):
     })
 
 
+def about(request):
+    return render(request, 'blog/about.html')
+
+
 def blogs_detail(request, blog_id):
     blog = get_object_or_404(Blog, id=blog_id)
     return render(request, 'blog/blog_detail.html', {
